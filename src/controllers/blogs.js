0c84e867d0f958ddf16755527210fa0c0c188de2
Tommi Koivula <tommi.koivula93@gmail.com>
2023-07-07 20:59:50 +0300

    error handling

--- src/controllers/blogs.js
+++ src/controllers/blogs.js
@@ -6,23 +6,33 @@ const blogFinder = async (req, res, next) => {
     next()
 }
 
+const errorHandler = (err, req, res, next) => {
+    if (err.name === 'SequelizeDatabaseError') {
+        return res.status(400).json({ error: 'bad request', message: err.message })
+    }
+    if (err.name === 'SequelizeValidationError') {
+        const messages = []
+        err.errors.forEach(e => messages.push(e.message))
+        return res.status(400).json({ error: 'bad request', messages })
+    }
+    return res.status(400).json({ error: 'bad request', message: err?.message })
+}
+
 router.get('/api/blogs', async (req, res) => {
     const blogs = await Blog.findAll();
     res.json(blogs)
 })
 
-router.post('/api/blogs', async (req, res) => {
+router.post('/api/blogs', async (req, res, next) => {
     try {
-        console.log(req.body)
         const blog = await Blog.create(req.body)
         return res.json(blog)
     } catch (error) {
-        console.log(error)
-        return res.status(400).json({ error })
+        next(error)
     }
 })
 
-router.get('/api/blogs/:id', blogFinder, async (req, res) => {
+router.get('/api/blogs/:id', blogFinder, (req, res) => {
     if (req.blog) {
         return res.json(req.blog)
     } else {
@@ -30,14 +40,29 @@ router.get('/api/blogs/:id', blogFinder, async (req, res) => {
     }
 })
 
-router.delete('/api/blogs/:id', blogFinder, async (req, res) => {
+router.delete('/api/blogs/:id', blogFinder, (req, res) => {
     if (req.blog) {
         req.blog.destroy()
         return res.sendStatus(200)
     } else {
         return res.sendStatus(404)
     }
-
 })
+
+router.put('/api/blogs/:id', blogFinder, async (req, res, next) => {
+    if (!req.blog) {
+        return res.sendStatus(404)
+    }
+    try {
+        const blog = req.blog
+        blog.likes = req.body?.likes
+        await blog.save()
+        return res.json(blog)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.use(errorHandler)
 
 module.exports = router
